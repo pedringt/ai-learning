e4dbf5380ecce344a1a82c41a2b4877f951e0359
Paige Edrington <paige.edrington@gmail.com>
2026-09-04 23:33:59 -0700

    Default unknown State hosts to production API

--- implementation-context-prototype/context-api.js
+++ implementation-context-prototype/context-api.js
@@ -1,5 +1,5 @@
 (() => {
-  const inferredBase = /(^|[-.])staging([-.]|$)|-git-|\.vercel\.app$/i.test(location.hostname)
+  const inferredBase = /(^|[-.])staging([-.]|$)|-git-/i.test(location.hostname)
     ? 'https://state-api-staging.onrender.com'
     : 'https://state-api-6waw.onrender.com';
   const base = window.STATE_API_BASE || document.documentElement?.dataset?.apiBase || inferredBase;
